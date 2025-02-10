91d7dfa80d656676f648daf74a0a40f36e5c2033
Hector Lopez <h.lopez@rimith.com>
2025-02-10 18:31:51 -0500

    config prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,12 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
