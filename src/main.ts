238b493ae604b226fee556483ccd02b0500ae8b8
Hector Lopez <h.lopez@rimith.com>
2025-02-11 13:32:12 -0500

    log env4

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('TesloShop');
   logger.log(`App running on port ${process.env.PORT}`)
-  logger.log(`App running on DB_HOST ${process.env.DB_HOST}`)
-  logger.log(`App running on DB_NAMEE ${process.env.DB_NAMEE}`)
-  logger.log(`App running on DB_USERNAMEE ${process.env.DB_USERNAMEE}`)
-  logger.log(`App running on DB_PORTT ${process.env.DB_PORTT}`)
-  logger.log(`App running on DB_PASSWORDD ${process.env.DB_PASSWORDD}`)
+  logger.log(`App running on DB_HOST ${process.env.CUSTOM_DB_HOST}`)
+  logger.log(`App running on DB_NAMEE ${process.env.CUSTOM_DB_NAME}`)
+  logger.log(`App running on DB_USERNAMEE ${process.env.CUSTOM_DB_USER}`)
+  logger.log(`App running on DB_PORTT ${process.env.CUSTOM_DB_PORT}`)
+  logger.log(`App running on DB_PASSWORDD ${process.env.CUSTOM_DB_PASS}`)
   const app = await NestFactory.create(AppModule);
   
 
